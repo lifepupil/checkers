006ef5c7e1da1f7efff04b38d03f8984b650914b
Christian Richard <lifepupil@gmail.com>
2015-04-09 22:37:36 -0700

    copy initialization controls from tic-tac-toe

--- client/index.js
+++ client/index.js
@@ -3,7 +3,26 @@
 $(document).ready(init);
 
 function init() {
-  // $('#reset').click(reset);
+  $('#reset').click(reset);
+  $('#start').click(start);
 
+}
 
+function reset() {
+  $('#chooser').show();
+  $('#players').hide();
+  $('td').css('background-color', 'white');
+}
+
+function start() {
+  $('#players').show();
+  var p1 = $('#p1-choose').val();
+  var p2 = $('#p2-choose').val();
+  $('#p1').css('background-color', p1);
+  $('#p2').css('background-color', p2);
+  $('#chooser').hide();
+
+  var rnd = Math.floor(Math.random() *2) + 1;
+  $('.player').removeClass('active');
+  $('#p' + rnd).addClass('active');
 }
